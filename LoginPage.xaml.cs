ba201259b14d6b4a48eaef59e539bac54fe5c420
Leenzery <ly@leenzery.dev>
2023-03-05 23:29:40 +0500

    upload project

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using OpenVkNetApi;
+using OpenVkNetApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace OpenVK
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class LoginPage : Page
+    {
+        private OvkApi api = new OvkApi();
+        private AuthorizedUser user;
+        private ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+        public LoginPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Вызывается перед отображением этой страницы во фрейме.
+        /// </summary>
+        /// <param name="e">Данные события, описывающие, каким образом была достигнута эта страница.
+        /// Этот параметр обычно используется для настройки страницы.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Подготовьте здесь страницу для отображения.
+
+            // TODO: Если приложение содержит несколько страниц, обеспечьте
+            // обработку нажатия аппаратной кнопки "Назад", выполнив регистрацию на
+            // событие Windows.Phone.UI.Input.HardwareButtons.BackPressed.
+            // Если вы используете NavigationHelper, предоставляемый некоторыми шаблонами,
+            // данное событие обрабатывается для вас.
+        }
+
+        private async void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+                if (!string.IsNullOrEmpty(loginBox.Text) && !string.IsNullOrEmpty(passwordBox.Password) && !string.IsNullOrEmpty(instanceBox.Text))
+                {
+                    user = await api.Authorize(loginBox.Text, passwordBox.Password, instanceBox.Text, ClientName: "");
+                    //сохранение токена и адреса инстанции
+                    localSettings.Values["instance"] = instanceBox.Text;
+                    localSettings.Values["token"] = user.AccessToken;
+                    Frame.Navigate(typeof(MainPage));
+                }
+                else
+                {
+                    
+                }
+            
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using OpenVkNetApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента пустой страницы см. по адресу http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace OpenVK
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static AuthorizedUser user;
+        private ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            user = new AuthorizedUser(localSettings.Values["token"].ToString(), localSettings.Values["instance"].ToString());
+
+        }
+        /// <summary>
+        /// Вызывается перед отображением этой страницы во фрейме.
+        /// </summary>
+        /// <param name="e">Данные события, описывающие, каким образом была достигнута эта страница.
+        /// Этот параметр обычно используется для настройки страницы.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
